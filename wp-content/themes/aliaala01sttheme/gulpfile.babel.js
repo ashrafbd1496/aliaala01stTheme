36f1c025d18b1a1c9e3fbe3552d079f2ba0bb6b9
ashrafbd1496 <asraf1496@gmail.com>
2022-10-16 12:38:36 +0600

    successfully converted gulp images src to dest folder

--- wp-content/themes/aliaala01sttheme/gulpfile.babel.js
+++ wp-content/themes/aliaala01sttheme/gulpfile.babel.js
@@ -1,16 +1,22 @@
 import gulp from 'gulp';
 import yargs from 'yargs';
-//import sass from 'gulp-sass';
-const sass = require('gulp-sass')(require('sass'));
+import sass from 'gulp-sass';
+//const sass = require('gulp-sass')(require('sass'));
 import cleanCSS from 'gulp-clean-css';
 import gulpif from 'gulp-if';
 import sourcemaps from 'gulp-sourcemaps';
+import imagemin from 'gulp-imagemin';
+
 const PRODUCTION = yargs.argv.prod;
 
 const paths = {
   styles: {
     src: ['src/assets/scss/bundle.scss','src/assets/scss/admin.scss'],
     dest: 'dist/asset/css'
+  },
+  images: {
+    src: 'src/assets/images/**/*.{jpg,jpeg,svg,gif,png}',
+    dest: 'dist/asset/images'
   }
 }
 
@@ -27,4 +33,10 @@ export const styles = ()=> {
 export const watch = () =>{
   gulp.watch('src/assets/scss/**/*.scss', styles);
 }
+export const images = () =>{
+  return gulp.src(paths.images.src)
+    .pipe(gulpif(PRODUCTION, imagemin()))
+    .pipe(gulp.dest(paths.images.dest));
+}
+
 //export default styles;
