a5d7f8608cacdeffa0ea7d66d27bd1f0fe73d204
ashrafbd1496 <asraf1496@gmail.com>
2022-12-12 12:22:18 +0000

    images will paste at dist asset folder when puts any image in src assets folder using command gulp copy

--- wp-content/themes/aliaala01sttheme/gulpfile.babel.js
+++ wp-content/themes/aliaala01sttheme/gulpfile.babel.js
@@ -9,33 +9,42 @@ import imagemin from 'gulp-imagemin';
 const PRODUCTION = yargs.argv.prod;
 
 const paths = {
-  styles: {
-    src: ['src/assets/scss/bundle.scss','src/assets/scss/admin.scss'],
-    dest: 'dist/asset/css'
-  },
-  images: {
-    src: 'src/assets/images/**/*.{jpg,jpeg,svg,gif,png}',
-    dest: 'dist/asset/images'
-  }
+    styles: {
+        src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'],
+        dest: 'dist/asset/css'
+    },
+    images: {
+        src: 'src/assets/images/**/*.{jpg,jpeg,svg,gif,png}',
+        dest: 'dist/asset/images'
+    },
+    other: {
+        src: ['src/assets/**/*', '!src/assets/{images,js,scss}', '!src/assets/{images,js,scss}/**/*'],
+        dest: ['dist/asset']
+    }
 }
 
 
-export const styles = ()=> {
-  return gulp.src(paths.styles.src)
-    .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulpif(PRODUCTION,cleanCSS({compatibility: 'ie8'})))
-    .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
-    .pipe(gulp.dest(paths.styles.dest));
+export const styles = () => {
+    return gulp.src(paths.styles.src)
+        .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulpif(PRODUCTION, cleanCSS({ compatibility: 'ie8' })))
+        .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+        .pipe(gulp.dest(paths.styles.dest));
 }
 
-export const watch = () =>{
-  gulp.watch('src/assets/scss/**/*.scss', styles);
+export const watch = () => {
+    gulp.watch('src/assets/scss/**/*.scss', styles);
 }
-export const images = () =>{
-  return gulp.src(paths.images.src)
-    .pipe(gulpif(PRODUCTION, imagemin()))
-    .pipe(gulp.dest(paths.images.dest));
+
+export const copy = () => {
+    return gulp.src(paths.other.src)
+        .pipe(gulp.dest(paths.other.dest));
+}
+export const images = () => {
+    return gulp.src(paths.images.src)
+        .pipe(gulpif(PRODUCTION, imagemin()))
+        .pipe(gulp.dest(paths.images.dest));
 }
 
 //export default styles;
